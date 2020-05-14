a7449b63b2adef988158e1033fdfd5d8e9915340
Daniel Pérez Porras <0619853431@uma.es>
2020-05-14 18:42:40 +0200

    He puesto mi pantalla de login

--- app/src/main/java/com/example/dressmeapp/MainActivity.java
+++ app/src/main/java/com/example/dressmeapp/MainActivity.java
@@ -2,9 +2,17 @@ package com.example.dressmeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText text_nombre;
+    private EditText text_contrasenia;
+    private Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +20,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    private void enlazarControles() {
 
+        text_nombre = findViewById(R.id.text_nombre);
+        text_contrasenia = findViewById(R.id.text_contrasenia);
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                irAMenuPrincipal();
+                //quitarTeclado(v);
+                // aqui pongo lo que hace el boton
+                //login(e_usuario.getText().toString(), e_password.getText().toString());
+            }
+        });
+
+    }
+
+    private void irAMenuPrincipal() {
+        Intent nuevaActividad = new Intent(this, MainActivity.class);
+        startActivity(nuevaActividad);
+        finish();
+    }
 
 }
