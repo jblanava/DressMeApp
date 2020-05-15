c94b1d855f663fb6d39d270a91f1e02490e6ab50
Daniel Pérez Porras <0619853431@uma.es>
2020-05-15 17:05:12 +0200

    Agregado EnlazarControles

--- app/src/main/java/com/example/dressmeapp/RegistroActivity.java
+++ app/src/main/java/com/example/dressmeapp/RegistroActivity.java
@@ -3,12 +3,42 @@ package com.example.dressmeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegistroActivity extends AppCompatActivity {
+
+    private EditText textNombre;
+    private EditText textContrasenia;
+    private EditText textConfirmaContrasenia;
+    private Button btnConfirmar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        enlazarControles();
     }
+
+    private void enlazarControles() {
+
+        textNombre = findViewById(R.id.usuarioCaja);
+        textContrasenia = findViewById(R.id.password);
+        textConfirmaContrasenia = findViewById(R.id.password2vez);
+        btnConfirmar = findViewById(R.id.buttonRegistro);
+
+        btnConfirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crearPerfil();
+            }
+        });
+
+    }
+
+    private void crearPerfil() {
+
+    }
+
 }
