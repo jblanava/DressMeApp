21de3bb124da992b076528436f0331f3a270b45e
jblanava <0619836326@uma.es>
2020-05-15 18:05:30 +0200

    funcionalidades botones login

--- app/src/main/java/com/example/dressmeapp/GestorBD.java
+++ app/src/main/java/com/example/dressmeapp/GestorBD.java
@@ -1,4 +1,41 @@
 package com.example.dressmeapp;
 
 public class GestorBD {
+
+    private String LoginUsuario;
+    private String LoginContrasena;
+    private String RegistroUsuario;
+    private String RegistroContrasena;
+
+
+    public GestorBD(){
+
+
+    }
+
+    public void IngresoPerfil(String u, String p){
+         //Clase Entrar
+
+
+    }
+
+    public void RegistroPerfil(String u, String p){
+      //clase Registro
+
+
+    }
+
+     private boolean EstaBD(String usuario){
+        //clase Registro
+
+        return false;
+     }
+
+     private boolean PassCorrecta(String usario, String password){
+        //clase Entrar
+
+
+        return false;
+     }
+
 }
