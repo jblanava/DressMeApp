551a8aa57a8cd436c537cddea4ad21dae5e7715d
jblanava <0619836326@uma.es>
2020-05-19 10:31:31 +0200

    funcionalidades botones login

--- app/src/main/java/com/example/dressmeapp/Activities/MostrarDatosActivity.java
+++ app/src/main/java/com/example/dressmeapp/Activities/MostrarDatosActivity.java
@@ -3,14 +3,46 @@ package com.example.dressmeapp.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.dressmeapp.BaseDatos.GestorBD;
 import com.example.dressmeapp.R;
 
 public class MostrarDatosActivity extends AppCompatActivity {
+
+    private TextView contrasenia;
+    private Button BotonPass;
+    private GestorBD gestor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar_datos); //Puede ser
     }
+
+/*
+    public void ocultarContrasenia{
+
+
+        BotonPass = findViewById(R.id.BotonPass);
+
+        BotonPass.setOnTouchListener(new OnTouchListener()){
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        editText.setInputType(InputType.TYPE_CLASS_TEXT);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        editText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                        break;
+                }
+                return true;
+            }
+        }
+    }
+}
+*/
 }
