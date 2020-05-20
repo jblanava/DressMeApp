6a070f982151d407a6cd475892dff4d98b0dea2a
Davidgc16 <davidgerecomi@uma.es>
2020-05-20 17:02:28 +0200

    Conjunto implementado

---
+++ app/src/main/java/com/example/dressmeapp/Objetos/Conjunto.java
@@ -0,0 +1,31 @@
+package com.example.dressmeapp.Objetos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Conjunto {
+    private List<Integer> idPrendas;
+
+    public Conjunto(){
+        idPrendas= new ArrayList<Integer>();
+    }
+
+    public void add(int idPrenda){
+        if(idPrendas.size()<6){
+            idPrendas.add(idPrenda);
+        }
+    }
+    public boolean estaId(int id){
+        if(idPrendas.contains(id)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public void remove(int idPrenda){
+        idPrendas.remove(idPrenda);
+    }
+
+
+}
