17d87846998f84880e151c75fa41e8019bdd8fd3
xmariia55x <mgm7cns@uma.es>
2020-05-30 17:12:51 +0200

    mas cosas part 2

---
+++ app/src/main/java/com/example/dressmeapp/BaseDatos/GestorBD2.java
@@ -0,0 +1,50 @@
+package com.example.dressmeapp.BaseDatos;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class GestorBD2 {
+    private static Context contexto; // TODO: eliminar en el futuro
+    private static String nombreBD = "dressmeapp18.db"; // Antonio ha cambiado a la BD__17
+
+    public GestorBD2(Context context)  // TODO: Eliminar?
+    {
+        contexto = context;
+    }
+
+    public static void CrearTalla(Context contexto, String talla) {
+        int id = obtenIDMaximoTalla(contexto);
+        String sentenciaSQL;
+        sentenciaSQL = "INSERT INTO TALLA (ID, TALLA) VALUES (";
+        sentenciaSQL += id + ",'" + talla.trim() + "')";
+
+        BaseDatos base = new BaseDatos(contexto, nombreBD);
+        SQLiteDatabase baseDatos;
+        baseDatos = base.getWritableDatabase();
+        baseDatos.execSQL(sentenciaSQL);
+        baseDatos.close();
+        base.close();
+    }
+
+    public static int obtenIDMaximoTalla(Context context) {
+        int resultado = 0;
+        String sentenciaSQL = "SELECT MAX(ID) AS MAXID FROM TALLA";
+
+        Cursor cursor;
+        BaseDatos base = new BaseDatos(context, nombreBD);
+        SQLiteDatabase baseDatos = base.getReadableDatabase();
+
+        cursor = baseDatos.rawQuery(sentenciaSQL, null);
+        if (cursor.moveToFirst()) {
+            do {
+                resultado = LibreriaBD.CampoInt(cursor, "MAXID");
+            } while (cursor.moveToNext());
+        }
+        resultado++;
+        baseDatos.close();
+        base.close();
+        cursor.close();
+        return resultado;
+    }
+}
