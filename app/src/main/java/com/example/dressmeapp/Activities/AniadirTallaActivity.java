f26b417ff77c00e6800400ac98ad90bcc6336a50
xmariia55x <mgm7cns@uma.es>
2020-05-30 17:20:07 +0200

    ya se puede añadir una nueva talla

--- app/src/main/java/com/example/dressmeapp/Activities/AniadirTallaActivity.java
+++ app/src/main/java/com/example/dressmeapp/Activities/AniadirTallaActivity.java
@@ -3,14 +3,36 @@ package com.example.dressmeapp.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.dressmeapp.BaseDatos.GestorBD2;
 import com.example.dressmeapp.R;
 
 public class AniadirTallaActivity extends AppCompatActivity {
-
+    EditText tallaNueva;
+    Button bGuardar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aniadir_talla);
+        getSupportActionBar().hide();
+        enlazarControles();
+    }
+
+    private void enlazarControles() {
+        bGuardar=findViewById(R.id.guardarTalla);
+        bGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                guardarTalla();
+            }
+        });
+    }
+
+    private void guardarTalla() {
+        String talla = tallaNueva.getText().toString();
+        GestorBD2.CrearTalla(this, talla);
     }
 }
