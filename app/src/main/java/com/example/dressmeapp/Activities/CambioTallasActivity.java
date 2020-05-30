5d8b4bb010c52bd7822ca1a482a0a08598fe10f9
xmariia55x <50658372+xmariia55x@users.noreply.github.com>
2020-05-30 17:10:29 +0200

    Creada la activity para mostrar y añadir tallas

--- app/src/main/java/com/example/dressmeapp/Activities/CambioTallasActivity.java
+++ app/src/main/java/com/example/dressmeapp/Activities/CambioTallasActivity.java
@@ -2,16 +2,35 @@ package com.example.dressmeapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.dressmeapp.R;
 
 public class CambioTallasActivity extends AppCompatActivity {
-
+    Button bAddTalla;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cambio_tallas);
         getSupportActionBar().hide();
+        enlazarControles();
+    }
+
+    private void enlazarControles() {
+        bAddTalla=findViewById(R.id.bAddTalla);
+        bAddTalla.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                aniadirTalla();
+            }
+        });
+    }
+
+    private void aniadirTalla(){
+        Intent nuevaTalla = new Intent(this,AniadirTallaActivity.class);
+        startActivity(nuevaTalla);
     }
 }
