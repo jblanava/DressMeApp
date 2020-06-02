a0b5636630598f62a3f896c446b1cdb49d64e786
Davidgc16 <davidgerecomi@uma.es>
2020-06-02 09:29:38 +0200

    Hechas clases Struct

--- app/src/main/java/com/example/dressmeapp/Objetos/Structs/PerfilBD.java
+++ app/src/main/java/com/example/dressmeapp/Objetos/Structs/PerfilBD.java
@@ -5,4 +5,21 @@ public class PerfilBD
     public int id;
     public String usuario;
     public String clave;
+
+    public PerfilBD(){}
+
+    public PerfilBD(String text, int baseID)
+    {
+        String[] campos = text.split(";");
+
+        id = Integer.parseInt(campos[0]) + baseID;
+        usuario = campos[1];
+        clave = campos[2];
+
+    }
+
+    public String toString()
+    {
+        return id + ";" + usuario + ";" + clave + "\n";
+    }
 }
--- app/src/main/java/com/example/dressmeapp/Objetos/Structs/PrendaBD.java
+++ app/src/main/java/com/example/dressmeapp/Objetos/Structs/PrendaBD.java
@@ -9,4 +9,24 @@ public class PrendaBD
     public int talla;
     public int visible;
     public int perfil;
+
+    public PrendaBD(){}
+
+    public PrendaBD(String text, int baseID)
+    {
+        String[] campos = text.split(";");
+
+        id = Integer.parseInt(campos[0]) + baseID;
+        nombre = campos[1];
+        color = Integer.parseInt(campos[2]);
+        tipo= Integer.parseInt(campos[3]);
+        talla= Integer.parseInt(campos[4]);
+        visible= Integer.parseInt(campos[5]);
+        perfil = Integer.parseInt(campos[6]);
+    }
+
+    public String toString()
+    {
+        return id + ";" + nombre + ";" + color + ";"+tipo+";"+talla+";"+visible+";"+perfil+ "\n";
+    }
 }
