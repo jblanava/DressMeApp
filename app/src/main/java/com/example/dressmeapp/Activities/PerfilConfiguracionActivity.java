3005bfdf04cafd0c70a647059d53bd1c3c047556
AntonioGC <antoniogc@uma.es>
2020-06-13 17:52:36 +0200

    Hecho: 	Actualizada nomenclatura PerfilConfiguacionActivity

--- app/src/main/java/com/example/dressmeapp/Activities/PerfilConfiguracionActivity.java
+++ app/src/main/java/com/example/dressmeapp/Activities/PerfilConfiguracionActivity.java
@@ -1,11 +1,5 @@
 package com.example.dressmeapp.Activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -15,6 +9,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.dressmeapp.BaseDatos.GestorBD;
 import com.example.dressmeapp.BaseDatos.GestorBDPerfil;
@@ -31,49 +31,49 @@ public class PerfilConfiguracionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_configuracion);
         getSupportActionBar().hide();
-        enlazarControles();
+        enlazar_controles();
     }
 
-    private void enlazarControles() {
+    private void enlazar_controles() {
 
         contexto = getApplicationContext();
-        Button cambioDatos = findViewById(R.id.botonCambiaDatos);
-        Button cambioContrasenia = findViewById(R.id.botonCambiaContrasenia);
-        Button exportarDatos = findViewById(R.id.botonExportarDatos);
-        Button borrarPerfil = findViewById(R.id.botonBorrarPerfil);
-        cambioContrasenia.setOnClickListener(new View.OnClickListener() {
+        Button bCambioDatos = findViewById(R.id.botonCambiaDatos);
+        Button bCambioContrasenia = findViewById(R.id.botonCambiaContrasenia);
+        Button bExportarDatos = findViewById(R.id.botonExportarDatos);
+        Button bBorrarPerfil = findViewById(R.id.botonBorrarPerfil);
+        bCambioContrasenia.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                irCambiarContrasenia();
+                ir_a_cambiar_contrasenia();
             }
         });
 
-        cambioDatos.setOnClickListener(new View.OnClickListener() {
+        bCambioDatos.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                MostrarDatos();
+                mostrar_datos();
             }
         });
 
-        exportarDatos.setOnClickListener(new View.OnClickListener() {
+        bExportarDatos.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                ExportarDatos();
+                exportar_datos();
             }
         });
 
-        borrarPerfil.setOnClickListener(new View.OnClickListener() {
+        bBorrarPerfil.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                irBorrarPerfil();
+                confirmar_borrar_perfil();
             }
         });
     }
 
-    protected void irBorrarPerfil() {
+    protected void confirmar_borrar_perfil() {
 
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        alertDialog.setMessage("¿Desea eliminar el perfil?");
-        alertDialog.setTitle("ATENCIÓN");
-        alertDialog.setIcon(android.R.drawable.ic_dialog_alert);
-        alertDialog.setCancelable(false);
-        alertDialog.setPositiveButton("Sí, estoy seguro", new DialogInterface.OnClickListener()
+        AlertDialog.Builder alert_dialog = new AlertDialog.Builder(this);
+        alert_dialog.setMessage("¿Desea eliminar el perfil?");
+        alert_dialog.setTitle("ATENCIÓN");
+        alert_dialog.setIcon(android.R.drawable.ic_dialog_alert);
+        alert_dialog.setCancelable(false);
+        alert_dialog.setPositiveButton("Sí, estoy seguro", new DialogInterface.OnClickListener()
         {
             public void onClick(DialogInterface dialog, int which)
             {
@@ -81,7 +81,7 @@ public class PerfilConfiguracionActivity extends AppCompatActivity {
                 //código Java si se ha pulsado sí
             }
         });
-        alertDialog.setNegativeButton("No, me lo he pensado mejor", new DialogInterface.OnClickListener()
+        alert_dialog.setNegativeButton("No, me lo he pensado mejor", new DialogInterface.OnClickListener()
         {
             public void onClick(DialogInterface dialog, int which)
             {
@@ -90,7 +90,7 @@ public class PerfilConfiguracionActivity extends AppCompatActivity {
             }
         });
 
-        alertDialog.show(); // esto es importante :D
+        alert_dialog.show(); // esto es importante :D
 
     }
 
@@ -105,25 +105,25 @@ public class PerfilConfiguracionActivity extends AppCompatActivity {
         startActivity(borrado);
     }
 
-    protected void MostrarDatos() {
+    protected void mostrar_datos() {
         //No sabemos que hase, preguntar a María :D
         Intent salto = new Intent(this,MostrarDatosActivity.class);
         startActivity(salto);
     }
 
-    protected void ExportarDatos() {
-        verificarYPedirPermisosDeAlmacenamiento();
+    protected void exportar_datos() {
+        pedir_permisos();
     }
 
-    protected void irCambiarContrasenia() {
-        Intent cambioContra = new Intent(this, ConfiguracionContraseniaActivity.class);
-        startActivity(cambioContra);
+    protected void ir_a_cambiar_contrasenia() {
+        Intent intent = new Intent(this, ConfiguracionContraseniaActivity.class);
+        startActivity(intent);
     }
 
 
-    private void verificarYPedirPermisosDeAlmacenamiento() {
-        int estadoDePermiso = ContextCompat.checkSelfPermission(PerfilConfiguracionActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if (estadoDePermiso == PackageManager.PERMISSION_GRANTED) {
+    private void pedir_permisos() {
+        int estado = ContextCompat.checkSelfPermission(PerfilConfiguracionActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (estado == PackageManager.PERMISSION_GRANTED) {
             new Exportador(this);
             Toast.makeText(PerfilConfiguracionActivity.this, "Se han exportado tus prendas en la carpeta de descargas", Toast.LENGTH_SHORT).show();
 
@@ -133,10 +133,11 @@ public class PerfilConfiguracionActivity extends AppCompatActivity {
                     0);
         }
     }
+
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == 0) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+    public void onRequestPermissionsResult(int request_code, @NonNull String[] permissions, @NonNull int[] grant_results) {
+        if (request_code == 0) {
+            if (grant_results.length > 0 && grant_results[0] == PackageManager.PERMISSION_GRANTED) {
                 new Exportador(this);
                 Toast.makeText(PerfilConfiguracionActivity.this, "Se han exportado tus prendas en la carpeta de descargas", Toast.LENGTH_SHORT).show();
 
