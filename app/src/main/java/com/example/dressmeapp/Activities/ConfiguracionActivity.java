b043f68199dcff00b25a3dc88829ee34691630cc
JoseAntonioArchilla <0619846163@uma.es>
2020-06-13 17:26:25 +0200

    modificando nomenclatura ConfiguracionActivity

--- app/src/main/java/com/example/dressmeapp/Activities/ConfiguracionActivity.java
+++ app/src/main/java/com/example/dressmeapp/Activities/ConfiguracionActivity.java
@@ -17,10 +17,10 @@ public class ConfiguracionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracion);
         getSupportActionBar().hide();
-        enlazaControles();
+        enlaza_controles();
     }
 
-    private void enlazaControles() {
+    private void enlaza_controles() {
         Button botonCambioPerfil = findViewById(R.id.botonCambioPerfil);
         Button botonCambioTallas = findViewById(R.id.botonCambioTallas);
         Button botonTipoColor = findViewById(R.id.botonTipoColor);
@@ -29,46 +29,46 @@ public class ConfiguracionActivity extends AppCompatActivity {
         botonCambioPerfil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cambiaPerfil();
+                cambia_perfil();
             }
         });
         botonTipoColor.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cambiaColor();
+                cambia_color();
             }
         }));
         botonCambioTallas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cambioTallas();
+                cambio_tallas();
             }
         });
         botonCombinaciones.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cambiaCombinaciones();
+                cambia_combinaciones();
             }
         });
     }
 
 
-    private void cambioTallas() {
+    private void cambio_tallas() {
     Intent cambio = new Intent(this,CambioTallasActivity.class);
     startActivity(cambio);
     }
 
-    private void cambiaColor() {
+    private void cambia_color() {
         Intent cambio = new Intent(this,CambioColorActivity.class);
         startActivity(cambio);
     }
 
-    private void cambiaCombinaciones() {
+    private void cambia_combinaciones() {
         Intent cambio = new Intent(this,AniadirCombinacion.class);
         startActivity(cambio);
     }
 
-    private void cambiaPerfil() {
+    private void cambia_perfil() {
         Intent cambio= new Intent(this,PerfilConfiguracionActivity.class);
         startActivity(cambio);
     }
